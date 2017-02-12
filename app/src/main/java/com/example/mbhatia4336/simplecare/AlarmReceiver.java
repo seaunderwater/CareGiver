5d2a5a85028541afe62ce6ec570b68eef6bc587e
mbhatia4336 <mbhatia4336@mbhatia4336s-MacBook-Pro.local>
2017-02-12 02:24:10 -0500

    automated notification

--- app/src/main/java/com/example/mbhatia4336/simplecare/AlarmReceiver.java
+++ app/src/main/java/com/example/mbhatia4336/simplecare/AlarmReceiver.java
@@ -1,7 +1,28 @@
 package com.example.mbhatia4336.simplecare;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
 /**
  * Created by mbhatia4336 on 2/12/17.
  */
-public class AlarmReceiver {
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = new NotificationCompat.Builder(context)
+                .setContentTitle("Random title")
+                .setContentText("Random text")
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0))
+                .build();
+
+        notificationManager.notify(0, notification);
+    }
 }
--- app/src/main/java/com/example/mbhatia4336/simplecare/MainActivity.java
+++ app/src/main/java/com/example/mbhatia4336/simplecare/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.mbhatia4336.simplecare;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.provider.AlarmClock;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +18,7 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SendData {
@@ -24,10 +29,19 @@ public class MainActivity extends AppCompatActivity implements SendData {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sendData = this;
-        Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
-        i.putExtra(AlarmClock.EXTRA_HOUR, 0);
-        i.putExtra(AlarmClock.EXTRA_MINUTES, 57);
-        startActivity(i);
+        AlarmManager alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+// set for 30 seconds later
+        alarmMgr.set(AlarmManager.RTC, Calendar.getInstance().getTimeInMillis() + 5000, alarmIntent);
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+//        mBuilder.setSmallIcon(R.drawable.ic_stat_name);
+//        mBuilder.setContentTitle("Notification Alert, Click Me!");
+//        mBuilder.setContentText("Hi, This is Android Notification Detail!");
+//        NotificationManager mNotifyMgr =
+//                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//// Builds the notification and issues it.
+//        mNotifyMgr.notify(1, mBuilder.build());
         //pushSearchToActivity(list, tasks);
         //new HTTPRequestTask().execute("http://192.168.43.53:8080/tasks");
         //String hello  = "hello";
